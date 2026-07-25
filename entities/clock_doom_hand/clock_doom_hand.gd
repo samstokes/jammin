486c0c7d412a8860e9470dc41fb88f288acce0e6
Sam Stokes <me@samstokes.co.uk>
2026-07-25 11:38:49 -0700

    stop when countdown reaches zero and emit signal

--- entities/clock_doom_hand/clock_doom_hand.gd
+++ entities/clock_doom_hand/clock_doom_hand.gd
@@ -4,7 +4,9 @@ extends "res://entities/clock_play_hand/clock_play_hand.gd"
 
 @export var doom_seconds = 60.0
 
-var doom_rads_per_second = TAU / doom_seconds
+signal doom
+
+@onready var doom_rads_per_second = TAU / doom_seconds
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,3 +17,12 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var delta_angle_doom = delta * doom_rads_per_second
 	angle += delta_angle_doom
+	
+	if angle >= TAU:
+		angle = TAU
+		doom.emit()
+		set_process(false)
+		
+func reset() -> void:
+	angle = 0.0
+	set_process(true)
