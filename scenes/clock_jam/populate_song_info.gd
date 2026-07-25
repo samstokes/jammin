b8d202697fedcf8e9ead602dac1b799124e3beb0
Michael Grossman <michaelg1223@gmail.com>
2026-07-25 15:31:44 -0700

    adding dynamic artist updates

--- scenes/clock_jam/populate_song_info.gd
+++ scenes/clock_jam/populate_song_info.gd
@@ -1,1 +1,7 @@
 extends Label
+
+func _ready() -> void:
+	AudioManager.get_node("AudioProcessing").connect("song", _on_new_song_info)
+
+func _on_new_song_info(title: String, artist: String, beats_per_measure: int):
+	text = title + "\n" + artist
