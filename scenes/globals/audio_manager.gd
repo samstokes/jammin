dffb5b60be22765d85add5a4e125c269ad68e120
Michael Grossman <michaelg1223@gmail.com>
2026-07-24 22:00:03 -0700

    adding support for dynamically choosing songs

--- scenes/globals/audio_manager.gd
+++ scenes/globals/audio_manager.gd
@@ -1,8 +1,56 @@
 extends Node
 
+var song_registry_path: String = "res://audio/song_registry.json"
+
+var song_id_to_info: Dictionary[int, SongInfo]
+
+func _ready() -> void:
+	if (!_load_library()):
+		print("Library Failed To Load")
+
 func play_sfx(audio_stream: AudioStream) -> void:
 	var player = AudioStreamPlayer.new()
 	player.stream = audio_stream
 	add_child(player)
 	player.play()
 	player.finished.connect(player.queue_free)
+
+func _on_audio_processing_finished() -> void:
+	load_song()
+
+func _load_library() -> bool:
+	song_id_to_info.clear()
+	if FileAccess.file_exists(song_registry_path):
+		var data_text = FileAccess.get_file_as_string(song_registry_path)
+		# Parse array from json
+		var data_parsed = JSON.parse_string(data_text)
+		for record in data_parsed:
+			var curr = SongInfo.new()
+			curr.title = record["title"]
+			curr.artist = record["artist"]
+			curr.bpm = record["bpm"]
+			curr.beats_per_measure = record["beats_per_measure"]
+			curr.UID = record["UID"]
+			song_id_to_info[record["id"]] = curr
+		print("Song Registry Read and stored.")
+		return true
+	else:
+		print("Song Registry Not Detected!")
+		return false
+
+
+func load_song():
+	var track_id = randi() % song_id_to_info.size() + 1
+	if !AudioManager.get_node("AudioProcessing").reset(song_id_to_info[track_id]):
+		print("AudioProcessing Node Not Reset")
+	else:
+		print("AudioProcessing Node reset")
+
+	
+class SongInfo:
+	var title: String
+	var artist: String
+	var bpm: int
+	var beats_per_measure: int
+	var UID: String
+	
--- scenes/globals/audio_processing.gd
+++ scenes/globals/audio_processing.gd
@@ -1,24 +1,24 @@
 extends AudioStreamPlayer
 
-@export var bmp := 120
+@export var bpm := 120
 @export var beats_per_measure := 4
-@export var song_name := "Stay Fresh"
-@export var artist_name := "Android52"
+@export var title := "Stay Fresh"
+@export var artist := "Android52"
 
-var song_pos = 0.0
-var song_pos_in_beats = 1
-var sec_per_beat = 60.0/bmp
-var last_reported_beat = 0
-var measure_num = 1
+var song_pos: float = 0.0
+var song_pos_in_beats: int = 1
+var sec_per_beat: float = 60.0/bpm
+var last_reported_beat: int = 0
+var measure_num: int = 1
 
 signal measure(position)
 signal beat(position)
-
+signal song(title, artist, beats_per_measure)
 
 # Called when the node enters the scene tree for the first time.
-# V1: play music immediately and set up the bmp dynamically from exported data
+# V1: play music immediately and set up the bpm dynamically from exported data
 func _ready() -> void:
-	sec_per_beat = 60.0/bmp
+	sec_per_beat = 60.0/bpm
 	play()
 
 # Update and track information every frame
@@ -29,32 +29,53 @@ func _physics_process(delta: float) -> void:
 		song_pos_in_beats = int(floor(song_pos/sec_per_beat))
 		_track_beat_and_measure()
 
-func _track_beat_and_measure():
+func _track_beat_and_measure() -> void:
 	if last_reported_beat < song_pos_in_beats:
 		if measure_num > beats_per_measure:
 			measure_num = 1
 		if measure_num == beats_per_measure:
-			emit_signal("measure", measure_num)
 			emit_signal("beat", song_pos_in_beats)
+			emit_signal("measure", song_pos_in_beats)
 		last_reported_beat = song_pos_in_beats
 		measure_num += 1
-		
 
-func get_measure_in_seconds():
+func get_measure_in_seconds() -> float:
 	return sec_per_beat*beats_per_measure
 	
 	# TAU rad * (1/ [ (X*sec/beat) * ( Y * beat / measure) ]
 	# = 2pi*rad/measure * (1/ [ (X*sec/beat) * ( Y * beat / measure) ] 
 	# = 2pi*rad/measure * [beat/ (X*sec)] * [measure/ (Y*beat)]
 	# = 2pi*rad / (X*Y) sec
-func get_rads_per_second():
+func get_rads_per_second() -> float:
 	return TAU/(sec_per_beat * beats_per_measure)
 
-func nearest_beat():
+func nearest_beat() -> int:
 	return round(song_pos / sec_per_beat)
 
-func delta_time_and_nearest_beat():
+func delta_time_and_nearest_beat() -> Vector2:
 	var nearest_beat_time = nearest_beat() * sec_per_beat
 	var abs_delta_to_nearest_beat = abs(song_pos - nearest_beat_time)
 	var nearest_beat_in_measure = int(round(nearest_beat_time /sec_per_beat)) % int(beats_per_measure)
 	return Vector2(abs_delta_to_nearest_beat, nearest_beat_in_measure)
+
+func reset(song_info: AudioManager.SongInfo) -> bool:
+	stop()
+	var file = AudioStreamMP3.load_from_file(song_info.UID)
+	if file == null:
+		print("Could not load next MP3")
+		play()
+		return false
+	stream = file
+	bpm = song_info.bpm
+	beats_per_measure = song_info.beats_per_measure
+	title = song_info.title
+	artist = song_info.artist
+	song_pos = 0.0
+	song_pos_in_beats = 1
+	sec_per_beat = 60.0/bpm
+	last_reported_beat = 0
+	measure_num = 1
+	emit_signal("song", title, artist, beats_per_measure)
+	play()
+	return true
+	
