bafcd4c5fa5468eea8915108447ce3a53a83e863
Sam Stokes <me@samstokes.co.uk>
2026-07-25 13:54:00 -0700

    fix borked indentation

--- scenes/globals/scene_manager.gd
+++ scenes/globals/scene_manager.gd
@@ -6,11 +6,11 @@ func transition_to_clock_jam() -> void:
 	var root = get_tree().root
 	var current_scene = get_tree().current_scene
 
-    # Remove the current scene and free its resources
+	# Remove the current scene and free its resources
 	root.remove_child(current_scene)
 	current_scene.queue_free()
 
-    # Instantiate the new game scene and set it as the current scene
+	# Instantiate the new game scene and set it as the current scene
 	# Here we could optionally pass parameters to the new scene if needed, such as the song id to play
 	var game_instance = game_scene.instantiate()
 	root.add_child(game_instance)
