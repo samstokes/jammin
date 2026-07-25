c0f081aae8d6f85a34af3a22bbbc6f26cc1b6d28
Sam Stokes <me@samstokes.co.uk>
2026-07-24 18:45:41 -0700

    add doom hand (no doom yet)

---
+++ entities/clock_doom_hand/clock_doom_hand.gd
@@ -0,0 +1,17 @@
+## Counts down toward game end over the course of doom_seconds.
+
+extends "res://entities/clock_play_hand/clock_play_hand.gd"
+
+@export var doom_seconds = 60.0
+
+var doom_rads_per_second = TAU / doom_seconds
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var delta_angle_doom = delta * doom_rads_per_second
+	angle += delta_angle_doom
