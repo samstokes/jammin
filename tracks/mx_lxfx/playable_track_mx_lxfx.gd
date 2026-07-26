f8e6fd2ad8dbbe7e95416bb12528e886a61ab030
Sam Stokes <me@samstokes.co.uk>
2026-07-25 18:36:32 -0700

    add track for mxlxfx, make it default, and bump doom clock so it's winnable

---
+++ tracks/mx_lxfx/playable_track_mx_lxfx.gd
@@ -0,0 +1,33 @@
+class_name PlayableTrackMxLxfx
+
+func track() -> PlayableTrack:
+	var playable_track = PlayableTrackBuilder.from_registry("mx lxfx !")
+
+	var message_to_spawn = "helloworld"
+
+	# Spawn a bunch of test data notes!
+	# One each other measure for 8 measures, counting down from a to z
+	var next_character_index = 0
+	# Song has a long intro with no beats
+	for measure_index in range(7, 15):
+		var effective_measure_index = measure_index - 7
+
+		for beat_index in range(4):
+			# Song starts on the off beat
+			if effective_measure_index == 0:
+				if beat_index < 2:
+					continue
+			
+			var character = message_to_spawn[next_character_index % message_to_spawn.length()]
+			next_character_index += 1
+
+			var spawn_time = PlayableTrackBuilder.BeatTime.new()
+			spawn_time.measure = measure_index
+			spawn_time.beat = beat_index
+
+			var note_time = PlayableTrackBuilder.BeatTime.new()
+			note_time.measure = measure_index + 1
+			note_time.beat = beat_index - 1
+			playable_track.spawn_note(spawn_time, note_time, character)
+		
+	return playable_track.build()
